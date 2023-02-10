063797c8594bd8d8d49ffaf95ebcc4d7745e20f0
shinghao <sohshinghao@gmail.com>
2023-02-10 17:40:40 +0800

    Image fix

--- my-portfolio/components/about/About.tsx
+++ my-portfolio/components/about/About.tsx
@@ -24,7 +24,7 @@ export default function About() {
           <Image
             src="shing-memoji-laptop.png"
             alt="Avatar of boy using macbook"
-            className="rounded-full"
+            className="rounded-full text-center"
           />
         </div>
         <hr className="row-start-3 col-span-7 h-px my-16 bg-gray-200"></hr>
