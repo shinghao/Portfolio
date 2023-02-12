5087186e62b764126bf23dad8499189b43530506
shinghao <sohshinghao@gmail.com>
2023-02-13 04:05:10 +0800

    Fix navbar minor positioning issue

--- my-portfolio/components/navbar/Navbar.tsx
+++ my-portfolio/components/navbar/Navbar.tsx
@@ -16,11 +16,11 @@ interface NavbarProps {
 function Navbar(prop: NavbarProps) {
   return (
     <nav
-      className="drop-shadow h-nav-bar  fixed z-10  w-screen bg-primary-bg-light dark:bg-primary-bg-dark
+      className="drop-shadow h-nav-bar fixed z-10  w-screen bg-primary-bg-light dark:bg-primary-bg-dark
     animate__animated animate__fadeInDownBig animate__delay-5s"
     >
-      <div className="grid grid-flow-col lg:auto-cols-fr items-center min-h-full w-11/12 m-auto">
-        <div className="flex flex-row gap-2 items-center">
+      <div className="grid grid-flow-col lg:auto-cols-fr items-center h-full w-11/12 m-auto ">
+        <div className="flex flex-row gap-x-2 items-center">
           <Link
             to="hero"
             smooth={true}
@@ -30,7 +30,7 @@ function Navbar(prop: NavbarProps) {
           >
             <h1>Shing Hao</h1>
           </Link>
-          <button className="xxm:p-3 lg:m-3 max-w-max border-2 rounded-xl hover:border-gray-200 dark:hover:border-gray-400 border-transparent hover:bg-gray-100 dark:hover:bg-gray-500">
+          <button className="xxm:p-3 lg:mx-3 max-w-max border-2 rounded-xl hover:border-gray-200 dark:hover:border-gray-400 border-transparent hover:bg-gray-100 dark:hover:bg-gray-500">
             {prop.darkMode ? (
               <FaCloudSun
                 className="text-yellow-500 text-icon-button "
