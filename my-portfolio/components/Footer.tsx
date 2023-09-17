9736067bd09d7bb82b09e748fb53594de515ded3
shinghao <sohshinghao@gmail.com>
2023-09-17 17:14:43 +0800

    Reduce space between footer social icon buttons

--- my-portfolio/components/Footer.tsx
+++ my-portfolio/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="flex flex-col items-center pb-8">
       <hr className="h-px mb-8 w-5/6 border-gray-300 dark:border-gray-500" />
-      <ul className="flex flex-row gap-8 pb-4 ">
+      <ul className="flex flex-row gap-2 pb-4 ">
         <SocialIconBtn link="https://github.com/shinghao" icon={FaGithub} />
         <SocialIconBtn
           link="https://www.linkedin.com/in/sohshinghao"
