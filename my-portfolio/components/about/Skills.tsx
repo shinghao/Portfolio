5ce7c53ad95800cbb05813c6ba05d5efdc12f064
shinghao <sohshinghao@gmail.com>
2023-10-26 00:27:51 +0800

    Update Skills.tsx

--- my-portfolio/components/about/Skills.tsx
+++ my-portfolio/components/about/Skills.tsx
@@ -3,7 +3,7 @@ import SkillCard from "../SkillCard";
 
 export default function Skills() {
   return (
-    <ul className="grid grid-cols-3 gap-4 xm:grid-cols-5 sm:grid-cols-7 lg:grid-flow-row lg:grid-rows-2 max-w-max m-auto ">
+    <ul className="grid grid-cols-3 gap-4 xxm:grid-cols-4 xm:grid-cols-5 sm:grid-cols-6 md:grid-cols-7 lg:grid-flow-row lg:grid-rows-2 max-w-max m-auto ">
       {mySkillsData.map((skill, index) => (
         <li key={index}>
           <SkillCard skill={skill}></SkillCard>
