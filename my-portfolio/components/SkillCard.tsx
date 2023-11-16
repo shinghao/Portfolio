0e8ecedb0687bf48b297e064302a9d798626eb7e
shinghao <sohshinghao@gmail.com>
2023-11-17 02:09:43 +0800

    Move tooltip trigger to SkillIcon

--- my-portfolio/components/SkillCard.tsx
+++ my-portfolio/components/SkillCard.tsx
@@ -1,26 +1,14 @@
-import { useState } from "react";
-import ToolTip from "./ToolTip";
 import skillMap from "@/data/skillMap";
 import SkillIcon from "./SkillIcon";
 
 export default function SkillCard(props: { skill: string }) {
-  const [showTip, setShowTip] = useState(false);
-  const handleMouseEnter = () => {
-    setShowTip(true);
-  };
-  const handleMouseLeave = () => {
-    setShowTip(false);
-  };
   const skillText = skillMap[props.skill];
   return (
     <div
       className="bg-slate-100 dark:bg-gray-400 shadow-skills-card dark:shadow-none 
                   rounded-3xl grid items-center justify-items-center
                   w-16 h-16"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
     >
-      {showTip && <ToolTip text={skillText}></ToolTip>}
       <SkillIcon src={props.skill} alt={skillText}></SkillIcon>
     </div>
   );
--- my-portfolio/components/SkillIcon.tsx
+++ my-portfolio/components/SkillIcon.tsx
@@ -1,14 +1,28 @@
 import Image from "next/image";
+import ToolTip from "./ToolTip";
+import { useState } from "react";
 
 export default function SkillIcon(props: { src: string; alt: string }) {
+  const [showTip, setShowTip] = useState(false);
+  const handleMouseEnter = () => {
+    setShowTip(true);
+  };
+  const handleMouseLeave = () => {
+    setShowTip(false);
+  };
   return (
-    <Image
-      className="w-12 text-center"
-      src={`/icons8-${props.src}-96.png`}
-      alt={props.alt}
-      width={96}
-      height={96}
-      quality={100}
-    />
+    <>
+      {showTip && <ToolTip text={props.alt}></ToolTip>}
+      <Image
+        className="w-12 text-center"
+        src={`/icons8-${props.src}-96.png`}
+        alt={props.alt}
+        width={96}
+        height={96}
+        quality={100}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      />
+    </>
   );
 }
