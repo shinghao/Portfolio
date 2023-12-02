53b7fe0fcd013365b976f0dba19388d5de3d88d2
shinghao <sohshinghao@gmail.com>
2023-12-02 15:49:01 +0800

    Fix: Move stylesheet links from index to document
    - This is because next.js do not recommend placing stylesheet links to next/head components

--- my-portfolio/pages/_document.tsx
+++ my-portfolio/pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Josefin+Slab&display=optional"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
